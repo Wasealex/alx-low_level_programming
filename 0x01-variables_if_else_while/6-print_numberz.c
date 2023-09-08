43026d58fe5ba3a5b0281b0c0caeafce42d661e3
Wasealex <wasealex27@gmail.com>
2023-09-08 14:24:31 +0300

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Return: always 0
+ */
+int main(void)
+{
+	int n = 48;
+
+	while (n < 58)
+	{
+		putchar (n);
+		n++;
+	}
+	putchar ('\n');
+
+	return (0);
+}
