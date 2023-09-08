d9536e911aace73f6ec231c4728a213817af3dca
Wasealex <wasealex27@gmail.com>
2023-09-08 21:40:23 +0300

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 /**
  *main - Entry mode
- *
  *Return: always 0 if success other wise error
  */
 int main(void)
 {
-	int a, b, c, d;
-
-	a = b = c = d = 47;
+	int a, b, c, d = a = b = c = d = 47;
 
 	while (a < 57)
 	{
@@ -30,10 +27,6 @@ int main(void)
 					{
 						continue;
 					}
-					if (b == d && a > c)
-					{
-						continue;
-					}
 					if (a > c)
 					{
 						continue;
@@ -43,6 +36,10 @@ int main(void)
 					putchar(' ');
 					putchar(c);
 					putchar(d);
+					if (a == 57 && b == 56 && c == 57 && d == 57)
+					{
+						break;
+					}
 					putchar(',');
 					putchar(' ');
 				}
