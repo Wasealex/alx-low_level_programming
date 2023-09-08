2d8151c4751140dbdb8e96ce7305047fc018804a
Wasealex <wasealex27@gmail.com>
2023-09-08 16:26:46 +0300

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,20 +7,24 @@
 int main(void)
 {
 	int a = 47;
-	int b = 48;
+	int b = 47;
 
 	while (a < 57)
 	{
 		a++;
-		while (b < 58)
+		while (b < 57)
 		{
+			b++;
+			if (a == b)
+			{
+				continue;
+			}
 			putchar (a);
 			putchar (b);
 			putchar (',');
 			putchar (' ');
-			b++;
 		}
-		b = 48;
+		b = 47;
 	}
 	putchar('\n');
 	return (0);
