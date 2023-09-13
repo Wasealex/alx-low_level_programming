6743e54f35f0ce677a25ab79516802bd244d34ab
Wasealex <wasealex27@gmail.com>
2023-09-13 20:17:08 +0300

    spacingcorrected

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,19 +18,25 @@ void times_table(void)
 			if (pr <= 9)
 			{
 				_putchar(pr + '0');
+				if (a <=9 && b == 9)
+				{
+					break;
+				}
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
 			}
 			if (pr > 9)
 			{
 				_putchar(pr / 10 + '0');
 				_putchar(pr % 10 + '0');
+				if (a <= 9 && b == 9)
+				{
+					break;
+				}
+				_putchar(',');
+				_putchar(' ');
 			}
-			if (a <= 9 && b == 9)
-			{
-				break;
-			}
-			_putchar(',');
-			_putchar(' ');
-			_putchar(' ');
 		}
 		_putchar('\n');
 	}
