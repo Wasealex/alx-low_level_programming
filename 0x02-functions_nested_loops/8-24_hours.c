148369f524cd7955f4eda32477aa016a54506932
Wasealex <wasealex27@gmail.com>
2023-09-13 16:41:14 +0300

    24hrs

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ *jack_bauer -  prints every minute of the day of Jack Bauer,
+ *starting from 00:00 to 23:59.
+ *
+ *Return: 24 hours
+ */
+void jack_bauer(void)
+{
+	int a, b, c, d;
+
+	for (a = 48; a < 51; a++)
+	{
+		for (b = 48; b < 58; b++)
+		{
+			for (c = 48; c < 54; c++)
+			{
+				for (d = 48; d < 58; d++)
+				{
+					_putchar(a);
+					_putchar(b);
+					_putchar(':');
+					_putchar(c);
+					_putchar(d);
+					_putchar('\n');
+					if (a > 50 && b < 54)
+					{
+						break;
+					}
+				}
+			}
+		}
+	}
+}
