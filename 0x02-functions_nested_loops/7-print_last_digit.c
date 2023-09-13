4f36919d6d5aa67b3460730ec2f94a7619f3ef8f
Wasealex <wasealex27@gmail.com>
2023-09-13 18:58:49 +0300

    lastdigitfix

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,7 +9,17 @@ int print_last_digit(int j)
 {
 	int last_digit;
 
-	j = j % 10;
-	last_digit = _putchar(j + '0');
-	return (last_digit);
+	if (j < 0)
+	{
+		j = -1 * j;
+		last_digit = j % 10;
+		_putchar(last_digit + '0');
+		return (last_digit);
+	}
+	else
+	{
+		j = j % 10;
+		last_digit = _putchar(j + '0');
+		return (last_digit);
+	}
 }
