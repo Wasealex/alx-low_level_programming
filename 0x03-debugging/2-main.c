5d5c6c49be5b4d0b2b7281d196153e306bd6d0e2
Wasealex <wasealex27@gmail.com>
2023-09-14 11:26:39 +0300

    checker12

--- 0x03-debugging/2-main.c
+++ 0x03-debugging/2-main.c
@@ -11,9 +11,9 @@ int main(void)
         int a, b, c;
         int largest;
 
-        a = 972;
-        b = -98;
-        c = 0;
+        c = 98;
+        b = 98;
+        a = 99;
 
         largest = largest_number(a, b, c);
 
