39defeb75c78ca637f75eebf2c4b737dfee38ec9
Wasealex <wasealex27@gmail.com>
2023-09-14 17:21:10 +0300

    for 0 < n < 10

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,53 @@
+#include "main.h"
+/**
+ *print_times_table - sets a time table of given number n
+ *@n: number to be done
+ *Return: returns a table if no error
+ */
+void print_times_table(int n)
+{
+	int a, b, c;
+
+	if (n <= 10 && n > 0)
+	{
+		for (a = 0; a <= n; a++)
+		{
+			for (b = 0; b <= n; b++)
+			{
+				c = a * b;
+				if (c / 10 == 0)
+				{
+					if (b == 0)
+					{
+						_putchar('0');
+					}
+					if (b != 0)
+					{
+						_putchar(' ');
+						_putchar(c + '0');
+						_putchar(' ');
+					}
+					if (b < n)
+					{
+						_putchar(',');
+						_putchar(' ');
+						_putchar(' ');
+					}
+				}
+				else
+				{
+					_putchar ((c / 10) + '0');
+					_putchar ((c % 10) + '0');
+					if (b < n)
+					{
+						_putchar(' ');
+						_putchar(',');
+						_putchar(' ');
+						_putchar(' ');
+					}
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
