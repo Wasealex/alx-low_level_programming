27c1a22cfbf7a0db00d23356a43acea75091c763
Wasealex <wasealex27@gmail.com>
2023-09-15 13:35:20 +0300

    prints number

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *print_numbers - prints numbers from 0 to 9
+ *
+ *
+ *Return: numbers if no error
+ */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+		_putchar(a);
+	_putchar('\n');
+}
