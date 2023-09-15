1fdb14d4c9202504207b9d4bcef2ee43576b84f7
Wasealex <wasealex27@gmail.com>
2023-09-15 14:14:41 +0300

    wrongoutput

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *more_numbers - prints numbers 10 times for 1 to 14
+ *
+ *Return: numbers if no error
+ *
+ */
+void more_numbers(void)
+{
+	int a, series;
+
+	for (series = 0; series < 10; series++)
+	{
+		for (a = 0; a <= 14; a++)
+		{
+			if (a / 10 == 0)
+				_putchar(a + '0');
+			else
+				_putchar((a % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
