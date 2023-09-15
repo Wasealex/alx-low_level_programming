68ed0753bc492ea3441ef1bccac0649a986328f6
Wasealex <wasealex27@gmail.com>
2023-09-15 18:00:18 +0300

    fizzbuzz fix

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,9 +8,13 @@ int main(void)
 {
 	int a;
 
-	for (a = 1; a <= 100; a++)
+	for (a = 1; a < 100; a++)
 	{
-		if (a % 3 == 0)
+		if (a % 5 == 0 && a % 3 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (a % 3 == 0)
 		{
 			printf("Fizz ");
 		}
@@ -18,15 +22,11 @@ int main(void)
 		{
 			printf("Buzz ");
 		}
-		else if (a % 5 == 0 && a % 3 == 0)
-		{
-			printf("FizzBuzz ");
-		}
 		else
 			printf("%d ", a);
 
 	}
-	printf("\n");
+	printf("Buzz\n");
 
 		return (0);
 }
