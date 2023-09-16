85e380154fcde95b1c235f0611f5719f51363498
Wasealex <wasealex27@gmail.com>
2023-09-16 14:18:05 +0300

    output

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,15 +8,15 @@ void print_triangle(int size)
 {
 	int a, b, c;
 
-	if (size >= 0)
+	if (size > 0)
 	{
 		for (a = 0; a < size; a++)
 		{
-			for (b = size - 1; b > 0; b--)
+			for (b = size - 1 - c; b > 0; b--)
 			{
 				_putchar(' ');
 			}
-			for (c = size; c > 0; c--)
+			for (c = a + 1; c > 0; c--)
 			{
 				_putchar('#');
 			}
