b7a0a0d92d8c27b205897166410b5be42f388eb8
Wasealex <wasealex27@gmail.com>
2023-09-16 14:08:46 +0300

    trialnglething

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -6,16 +6,22 @@
  */
 void print_triangle(int size)
 {
-	int a;
+	int a, b, c;
 
 	if (size >= 0)
 	{
 		for (a = 0; a < size; a++)
 		{
-			_putchar('#');
+			for (b = size - 1 - a; b > 0; b--)
+			{
+				putchar(' ');
+			}
+			for (c = a + 1; c > 0; c--)
+			{
+				_putchar('#');
+			}
 			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 	else
 		_putchar('\n');
