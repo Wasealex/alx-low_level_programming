4e2fda7ef71d757eb512ba2e91a338b4a66d00e5
Wasealex <wasealex27@gmail.com>
2023-09-18 19:42:33 +0300

    in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *print_rev - output a string in reverse
+ *@s: string
+ *
+ *Return: a string
+ */
+void print_rev(char *s)
+{
+	int print = 0;
+	int total = 0;
+
+	while (s[print] != '\0')
+	{
+		total++;
+		print++;
+	}
+	for (; total >= 0; total--)
+	{
+		_putchar(s[total]);
+	}
+	_putchar('\n');
+}
