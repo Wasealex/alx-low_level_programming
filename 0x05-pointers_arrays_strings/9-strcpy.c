b57563bce4afa2014c7ec4f93b3a6c6c59625d17
Wasealex <wasealex27@gmail.com>
2023-09-19 00:34:44 +0300

    null fix

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,9 +9,10 @@ char *_strcpy(char *dest, char *src)
 {
 	int a;
 
-	for (a = 0; src[a] != '\0' || dest[a] != '\0'; a++)
+	for (a = 0; src[a] != '\0'; a++)
 	{
 		dest[a] = src[a];
 	}
+	dest[a] = '\0';
 	return (dest);
 }
