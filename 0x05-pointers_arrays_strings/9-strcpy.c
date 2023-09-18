578085e381cec4ac8b0dfe09a6898e46abb40133
Wasealex <wasealex27@gmail.com>
2023-09-19 00:23:00 +0300

    betty

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -2,16 +2,20 @@
 /**
  *_strcpy - to copy from source string to desitination string
  *@dest: string 2
- *@source: string 1
+ *@src: string 1
  *Return: dest
  */
 char *_strcpy(char *dest, char *src)
 {
-	int a;
+	int a, b, len;
 
 	for (a = 0; src[a] != '\0'; a++)
 	{
-		dest[a] = src[a];
+		len = a;
+	}
+	for (b = 0; b <= len + 1; b++)
+	{
+		dest[b] = src[b];
 	}
 	return (dest);
 }
