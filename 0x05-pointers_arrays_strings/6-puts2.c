3c2246a7710466809cccf7fc82f6306090362a3e
Wasealex <wasealex27@gmail.com>
2023-09-18 23:14:46 +0300

    lets see

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *puts2 - print every other character
+ *@str: string
+ *Return: string when success
+ *
+ */
+void puts2(char *str)
+{
+	int a, len, b;
+
+	for (a = 0; str[a] != '\0'; a++)
+	{
+		len = a;
+	}
+
+	for (b = 0; b <= len ; b++)
+	{
+		if (b % 2 == 0)
+		{
+			_putchar(str[b]);
+		}
+	}
+	_putchar('\n');
+}
