a62cc5284b51b72a87ebead2f0f9b2b1a6360a6d
Wasealex <wasealex27@gmail.com>
2023-09-20 11:19:08 +0300

    uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *String_toupper - letters to uppper case
+ *@str: a string
+ *
+ *Return: a pointer to char (string)
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] > 'a' && str[i] < 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+		else
+			str[i] = str[i];
+	}
+	return (str);
+}
