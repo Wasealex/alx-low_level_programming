e7803716977339cbbb6aa0d72ef9e3a112609a02
Wasealex <wasealex27@gmail.com>
2023-09-20 09:53:49 +0300

    fixed null ending

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,11 +8,14 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i, dl, sl, temp;
+	int i;
 
-	for (i = 0; src[i] != '\0' || i < n; i++)
+	for (i = 0; src[i] != '\0' && i < n; i++)
 	{
 		dest[i] = src[i];
 	}
-	dest[i] = '\0';
+	for (; i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
 }
