5dac7d7df00604bc3542d85fd94b728f92517528
Wasealex <wasealex27@gmail.com>
2023-09-21 11:41:18 +0300

    rev fixed without null

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,10 +9,10 @@ void reverse_array(int *a, int n)
 {
 	int i, tmp;
 
-	for (i = 0; a[i] != '\0' && i < n / 2; i++)
+	for (i = 0; i < n / 2; i++)
 	{
 		tmp = a[i];
-		a[i] = a[n / 2 - i - 1];
-		a[n / 2 - 1 - i] = tmp;
+		a[i] = a[n - i - 1];
+		a[n - 1 - i] = tmp;
 	}
 }
