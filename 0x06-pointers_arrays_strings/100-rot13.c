259bfc36292afb3be6a0d61eab0c6b7e07837b51
Wasealex <wasealex27@gmail.com>
2023-09-21 13:07:43 +0300

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *rot13 - rot13 code language
+ *@s: string
+ *
+ *Return: a pointer to char
+ */
+char *rot13(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] <= 'z' && s[i] >= 'n')
+		{
+			s[i] = s[i] - 13;
+		}
+	}
+	return (s);
+}
