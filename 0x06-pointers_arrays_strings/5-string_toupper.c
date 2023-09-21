6fdc21e7eb2e6ed328c34a8a835b1687576cea49
Wasealex <wasealex27@gmail.com>
2023-09-21 11:44:04 +0300

    included a and z

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -11,7 +11,7 @@ char *string_toupper(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] > 'a' && str[i] < 'z')
+		if (str[i] >= 'a' && str[i] <= 'z')
 		{
 			str[i] = str[i] - 32;
 		}
