e9150d5454b635b3a0c1b6bb58f30027a4bd3477
Wasealex <wasealex27@gmail.com>
2023-09-25 23:04:58 +0300

    garbage value fixed

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,12 +9,13 @@ void print_diagsums(int *a, int size)
 {
 	int i, sum1, sum2;
 
-	sum1 = sum2 = 0;
+	sum1 = 0;
+	sum2 = 0;
 
 	for (i = 0; i < size; i++)
 	{
 		sum1 = sum1 + *(a + i * size + i);
-		sum2 = sum2 + *(a + i * size - i - 1);
+		sum2 = sum2 + *(a + i * size + size - i - 1);
 	}
-	printf("%d, %d\n", sum1, sum2);
+	printf("%i, %i\n", sum1, sum2);
 }
