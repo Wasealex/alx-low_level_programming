00a4873c20bfbe348448c158c9d96078630d2bf3
Wasealex <wasealex27@gmail.com>
2023-09-25 19:21:33 +0300

    count - 5

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *_strspn - gets the length of the prefix substring
+ *@s: string
+ *@accept: a string to be matched
+ *Return: an intiger of the length
+ *
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j;
+	unsigned int count = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (accept[j] == s[i])
+				count++;
+		}
+	}
+	return (count - 2);
+}
