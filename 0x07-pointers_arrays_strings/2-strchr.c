1995f24294364997633d715e97f5b399b5e265d5
Wasealex <wasealex27@gmail.com>
2023-09-25 14:47:21 +0300

    trial1

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *_strchr - identify a character
+ *@s: a string
+ *@c: a character
+ *Return: pointer to char
+ */
+char *_strchr(char *s, char c)
+{
+	if (*s == c)
+		return (s +1);
+	else
+		return (s);
+}
