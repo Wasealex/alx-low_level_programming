d6b9a87cf07c1ec088213319c829cd5aaed917b6
Wasealex <wasealex27@gmail.com>
2023-09-25 23:09:40 +0300

    modi

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *set_string - set a value of pointer to char
+ *@s: content
+ *@to: string
+ *Return: 0 if success
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
