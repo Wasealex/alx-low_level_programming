9eba2ddcef60549b8bf7f6b10dc3c006d1e65fcc
Wasealex <wasealex27@gmail.com>
2023-10-02 23:17:26 +0300

    negative number

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -8,12 +8,18 @@
  */
 int main(int argc, char **argv)
 {
+	int cents;
+
+	cents = atoi(argv[1]);
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
+	if (cents < 0)
+		printf("0\n");
 	else
-		printf("%d\n", atoi(argv[1]));
+		printf("%d\n", cents);
+
 	return (0);
 }
