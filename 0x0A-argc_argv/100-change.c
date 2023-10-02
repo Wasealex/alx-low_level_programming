cd9d4d53e603fabd605a0dd2925106c332ce03c7
Wasealex <wasealex27@gmail.com>
2023-10-02 23:33:56 +0300

    segmentation

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,16 +10,15 @@ int main(int argc, char **argv)
 {
 	int cents;
 
-	cents = atoi(argv[1]);
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
+	cents = atoi(argv[1]);
 	if (cents < 0)
 		printf("0\n");
 	else
 		printf("%d\n", cents);
-
 	return (0);
 }
