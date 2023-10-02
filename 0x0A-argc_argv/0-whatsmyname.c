5dd08500f7925ff8cf18bf327d4f62ac5311b5b6
Wasealex <wasealex27@gmail.com>
2023-10-02 19:06:01 +0300

    betty fix

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -11,10 +11,10 @@ int main(int argc, char **argv)
 	int i;
 
 	for (i = 0; (*(argv[argc - 1] + i)) != '\0'; i++)
-		{
-			_putchar(*(argv[0] + i));
-		}
+	{
+		_putchar(*(argv[0] + i));
+	}
 	_putchar('\n');
-	
+
 	return (0);
 }
