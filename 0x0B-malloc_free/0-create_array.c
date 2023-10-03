a6550dcca241c2185a5019923b42bcebef163241
Wasealex <wasealex27@gmail.com>
2023-10-03 21:28:55 +0300

    first time using malloc

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ *create_array - creates an array of chars initialiezed with specific char
+ *@size: size of the array
+ *@c: a character
+ *Return: a pointer otherwise NULL
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *arr;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		arr = malloc(size * sizeof(char));
+
+		for (i = 0; i < size; i++)
+		{
+			*(arr + i) = c;
+		}
+	}
+	return (arr);
+}
---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+/*header files*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+/*prototypes*/
+int _putchar(char c);
+char *create_array(unsigned int size, char c);
+
+#endif /*MAIN_H_*/
