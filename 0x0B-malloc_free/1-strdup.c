e1dc9ccf6620fdf9efd9370ba4022d1b99291de1
Wasealex <wasealex27@gmail.com>
2023-10-03 22:23:25 +0300

    copy a string to heap

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ *_strdup - copy a string to a newlly allocated memory
+ *@str: a string
+ *Return: a string otherwise NULL
+ */
+char *_strdup(char *str)
+{
+	int i;
+	char *nstr;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	nstr = malloc(sizeof(*str));
+
+	if (nstr != NULL)
+	{
+		for (i = 0; *(str + i) != '\0'; i++)
+		{
+			*(nstr + i) = *(str + i);
+		}
+		return (nstr);
+	}
+	else
+		return (NULL);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -8,5 +8,6 @@
 /*prototypes*/
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
 
 #endif /*MAIN_H_*/
