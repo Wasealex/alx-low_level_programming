b4a23654509e8e3dae4a35957d9169b41fc3e29e
Wasealex <wasealex27@gmail.com>
2023-10-04 12:33:08 +0300

    null errro

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,9 +12,9 @@ char *str_concat(char *s1, char *s2)
 	char *str;
 
 	if (s1 == NULL)
-		s1 = " ";
+		s1 = "";
 	if (s2 == NULL)
-		s2 = " ";
+		s2 = "";
 
 	len1 = strlen(s1);
 	len2 = strlen(s2);
