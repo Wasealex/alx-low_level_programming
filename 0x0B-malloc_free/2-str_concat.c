db477991308f998da0f030ff0293c0833d3e662a
Wasealex <wasealex27@gmail.com>
2023-10-04 12:14:16 +0300

    error

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,13 +17,17 @@ char *str_concat(char *s1, char *s2)
 
 	if (str == NULL)
 		return (NULL);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	for (i = 0; i < len1 + len2; i++)
 	{
 		str[i] = s1[i];
 	}
 	for (i = 0; i <= len2; i++)
 	{
-		str[len1 +i] = s2[i];
+		str[len1 + i] = s2[i];
 	}
 	return (str);
 }
