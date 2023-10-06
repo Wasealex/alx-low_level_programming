29e2295cf990a02f879898cd87b2ceabe14a59bc
Wasealex <wasealex27@gmail.com>
2023-10-06 23:07:44 +0300

    trial 2

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -28,7 +28,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 	if (ptr == NULL)
 	{
-		return (ptr1);
+		return (malloc(new_size));
 	}
 	if (new_size == 0 && ptr != NULL)
 	{
