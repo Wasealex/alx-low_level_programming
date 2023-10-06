d16a5ddec0eb262d70298ae746f0f58bb4cc7f0a
Wasealex <wasealex27@gmail.com>
2023-10-07 00:02:36 +0300

    betty fix

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -3,14 +3,14 @@
  *main - entry point
  *@argc:  argument count
  *@argv: argument vector
- *
+ *Return: 0 if success
  */
 int main(int argc, char **argv)
 {
-	if (argc <= 2 || atoi(argv[1]) == 0 || atoi(argv[2]) == 0)
+	if (argc <= 2 || argc > 3 || atoi(argv[1]) == 0 || atoi(argv[2]) == 0)
 	{
 		printf("Error\n");
-		exit (98);
+		exit(98);
 	}
 	else
 		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
