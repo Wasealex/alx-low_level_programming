f817d479fb2a57066883d188057eeff29ef06edd
Wasealex <wasealex27@gmail.com>
2023-10-09 20:36:10 +0300

    header file created

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _dog_h_
+#define _dog_h_
+/**
+ *struct dog - defines information of dog with dog owner
+ *@name: name of the dog
+ *@age: age of the dog
+ *@owner: name of dog owner
+ *Description - this is a list of information on dog name and age
+ *in addition it will take dog owner name.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /*_dog_h_*/
