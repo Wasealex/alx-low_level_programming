61bc3b8e05b98dd9bc5e8dbcc9180215e3bec821
Wasealex <wasealex27@gmail.com>
2023-10-09 19:58:30 +0300

    trial

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,5 +1,5 @@
 #ifndef _3_function_like_macro_h
 #define _3_function_like_macro_h
-#define ABS(x) ((x) < (0)) ? ((-1) * (x)) : ((1) * (x))
+#define ABS(x) ((x < 0) ? ((-1) * (x)) : (x))
 /*ABS(x)*/
 #endif /*_3_function_like_macro_h*/
