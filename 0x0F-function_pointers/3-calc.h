6e305eeb4b2704220bd32a15e2956d306bbc9203
Wasealex <wasealex27@gmail.com>
2023-10-12 01:30:12 +0300

    tr

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -2,16 +2,14 @@
 #define _calc_h_
 /*headers*/
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
-#include <unistd.h>
 /*protytypes*/
-int (*get_op_func(char *s))(int, int);
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
+int (*get_op_func(char *s))(int, int);
 /*structures and typedef*/
 /**
  *struct op - Struct op
