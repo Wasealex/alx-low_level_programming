28bba7afde6a149265d94371d3d73a4fedc71db3
Wasealex <wasealex27@gmail.com>
2023-10-12 00:00:06 +0300

    main functions lay out

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,37 @@
+#include "calc.h"
+/**
+ *main - entry point for calculator
+ *@argc: total count of argument should be 4
+ *@argv: vactor arguments
+ *Return: 0 if success
+ */
+int main(int argc, int **argv)
+{
+	int num1, num2, result;
+	char *op;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	result = 0;
+
+	num1 = atoi(argv[1]);
+	op = atoi(argv[2]);
+	num2 = atoi(argv[3]);
+	if (op)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((*op == '\' && num2 == 0) || (*op == '%' && num2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	result = get_op_func(op)(num1, num2);
+	printf("%d\n", result);
+
+	return (0);
+}
