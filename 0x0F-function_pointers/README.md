75df82a448c9e141a1dd0b851c19e8959cbdbd2c
Wasealex <wasealex27@gmail.com>
2023-10-11 21:21:14 +0300

    README and header file

---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,2 @@
+this project is about pointer functions
+it can be very useful in call back functions 
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,9 @@
+#ifndef _function_pointers_h_
+#define _function_pointers_h_
+/*header file*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+/*prtotype*/
+#endif /*function_pointers.h*/
