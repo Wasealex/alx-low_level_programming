226aaf21d35c91f3973437ea26597decae5f5337
Wasealex <wasealex27@gmail.com>
2023-10-12 01:56:15 +0300

    not allowed to use for loop

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -17,12 +17,15 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i;
 
-	for (i = 0; i < 5; i++)
+	i = 0;
+
+	while(i < 5)
 	{
 		if (*(ops[i].op) == *s)
 		{
 			return (ops[i].f);
 		}
+		i++;
 	}
 	return (NULL);
 }
