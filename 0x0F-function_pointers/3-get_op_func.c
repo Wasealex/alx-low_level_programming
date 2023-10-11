bc5d9ac2fe6788e68486df53cded2f38a61c864e
Wasealex <wasealex27@gmail.com>
2023-10-12 01:14:28 +0300

    hopefully it works

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -17,7 +17,12 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i;
 
-	for (i = 0; ops[i].op != NULL && *(ops[i].op) != *s; i++)
-		return (ops[i].f);
+	for (i = 0; ops[i].op != NULL; i++)
+	{
+		if (strcmp(ops[i].op, s) == 0)
+		{
+			return (ops[i].f);
+		}
+	}
 	return (NULL);
 }
