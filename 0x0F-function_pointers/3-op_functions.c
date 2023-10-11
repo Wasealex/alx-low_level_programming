b4d41eccd5a72452d097e174c3c95aa1568b7e89
Wasealex <wasealex27@gmail.com>
2023-10-12 00:19:22 +0300

    layout for op function

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "calc.h"
+/**
+ *op_add - add 2 number
+ *@a: first number
+ *@b: second number
+ *Return: result
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ *op_sub - sub 2 number
+ *@a: first number
+ *@b: second number
+ *Return: result
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ *op_mul - mul 2 number
+ *@a: first number
+ *@b: second number
+ *Return: result
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ *op_div - divide 2 number
+ *@a: first number
+ *@b: second number
+ *Return: result
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ *op_mod - modulo 2 number
+ *@a: first number
+ *@b: second number
+ *Return: result
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
