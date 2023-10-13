b639bc1b49f863dacf585566e0033ce3e75405a3
Wasealex <wasealex27@gmail.com>
2023-10-13 05:15:50 +0300

    else

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -34,7 +34,10 @@ void print_all(const char * const format, ...)
 				break;
 			case 's':
 				s = va_arg(args, char *);
-				printf("%s", s);
+				if (s != NULL)
+					printf("%s", s);
+				else
+					printf("(nil)");
 				break;
 			}
 			ptr++;
