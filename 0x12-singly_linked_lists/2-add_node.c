8eb933c7258ce631ed7a59b6b79548dcf358d881
Wasealex <wasealex27@gmail.com>
2023-10-19 22:20:00 +0300

    adding node at the begining

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ *add_node - adds list at the begining
+ *@head: address of head pointer pointing to the first node
+ *@str: string which needs to be duplicated
+ *Return: address of new element , NULL if failed
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	/*list_t is a struct has 2 data(1- 'str' 2-'len) and pointer ('next)*/
+	list_t *first;
+
+	first = malloc(sizeof(list_t));
+	if (first == NULL)
+		return (NULL);
+	first->str = strdup(str);
+	first->len = strlen(first->str);
+	first->next = *head;
+
+	*head = first;
+	return (first);
+}
