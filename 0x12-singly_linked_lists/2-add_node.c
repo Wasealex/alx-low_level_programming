104a342f57d9e7196c9e3e9262f2585b9f7e7185
Wasealex <wasealex27@gmail.com>
2023-10-19 23:11:18 +0300

    lets free it first

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,12 +10,14 @@ list_t *add_node(list_t **head, const char *str)
 	/*list_t is a struct has 2 data(1- 'str' 2-'len) and pointer ('next)*/
 	list_t *first;
 
-	if (str == NULL)
-		return (NULL);
-
 	first = malloc(sizeof(list_t));
 	if (first == NULL)
 		return (NULL);
+	if (str == NULL)
+	{
+		free(first);
+		return (NULL);
+	}
 	first->str = strdup(str);
 	first->len = strlen(first->str);
 	first->next = *head;
