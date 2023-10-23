4c6eb583a710ca6cfd1a3bd45e06c1b9c8cd7387
Wasealex <wasealex27@gmail.com>
2023-10-23 12:41:00 +0300

    printing lists and returning number of node

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ *print_listint - prints content of lists
+ *@h: head pointer pointing to first element of listint_s
+ *
+ *Return: number of nodes
+ *
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t count;
+	const listint_t *tmp;
+
+	tmp = h;
+
+	while (tmp != NULL)
+	{
+		count++;
+		printf("%d\n", tmp->n);
+		tmp = tmp->next;
+	}
+	return (count);
+}
---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,23 @@
+#ifndef LISTS_H
+#define LISTS_H
+/*header files*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <unistd.h>
+#include <string.h>
+/*typdef and struct types*/
+/**
+ *struct listint_s - singly linked list
+ *@n: integer
+ *@next: points to the next node
+ *Description: singly linked list node structure
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+/*function prototypes */
+size_t print_listint(const listint_t *h);
+#endif /*LISTS_H*/
