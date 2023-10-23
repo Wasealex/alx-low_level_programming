35a782c4a44402c6b288d95df47b604107ca5535
Wasealex <wasealex27@gmail.com>
2023-10-23 18:24:34 +0300

    if new node is NULL

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -10,6 +10,8 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	listint_t *newnode;
 
 	newnode = malloc(sizeof(listint_t));
+	if (newnode == NULL)
+		return (NULL);
 	newnode->n = n;
 	newnode->next = *head;
 	*head = newnode;
