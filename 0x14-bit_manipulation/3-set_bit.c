89a6e91ae6d687d3c3b657ceb80f37acc50cedc1
Wasealex <wasealex27@gmail.com>
2023-10-26 15:27:43 +0300

    trial 4

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *set_bit - set 1 at given index
+ *@n: address of a given number
+ *@index: index at which to set 1
+ *Return: 1 if successfull otherwise -1
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (n == NULL || index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+	n[index] = 1;
+
+	return (1);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -10,4 +10,5 @@ unsigned int power(unsigned int exponent);
 void print_binary(unsigned long int n);
 int _putchar(char c);
 int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
 #endif /*MAIN_H*/
