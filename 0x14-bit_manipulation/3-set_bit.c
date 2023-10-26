bcacb654450cbd254da5cc7d1960c9183b220b8f
Wasealex <wasealex27@gmail.com>
2023-10-26 15:28:23 +0300

    trial 6

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -7,7 +7,7 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (n == NULL || index >= (sizeof(unsigned long int) * 8))
+	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
 	n[index] = 1;
 
