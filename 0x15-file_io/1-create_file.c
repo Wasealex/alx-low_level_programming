87617345de780acbcb5aef95afcabe94c86b8944
Wasealex <wasealex27@gmail.com>
2023-10-31 10:32:47 +0300

    for textcontent NULL

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,7 +12,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 	if (text_content == NULL)
-		return (1);
+		text_content = "";
 	fd0 = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	fd2 = write(fd0, text_content, strlen(text_content));
 	if (fd2 == -1 || fd0 == -1)
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,7 +12,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 	if (text_content == NULL)
-		return (1);
+		text_content == "";
 	fd0 = open(filename, O_WRONLY | O_APPEND);
 	fd2 = write(fd0, text_content, strlen(text_content));
 
