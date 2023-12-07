dab454e3d500bf9ec003373d5766c7cae02fa107
Wasealex <wasealex27@gmail.com>
2023-12-07 19:35:17 +0300

    fixed for head null and idx 0 and last idx and outof idx

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -3,38 +3,53 @@
  *insert_dnodeint_at_index - inserts a new node at a given position
  *@h: a head pointer pointing to the first node
  *@idx: index poistion
+ *@n: data to be inserted
  *Return: the address of the new node, or NULL if it failed
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node = NULL, *travel = NULL, *current = NULL;
+	dlistint_t *new_node = NULL, *travel = NULL;
 	unsigned int count = 0;
 
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
-	{
 		return (NULL);
-	}
 	new_node->prev = NULL;
 	new_node->n = n;
 	new_node->next = NULL;
-	travel = *h;
-	while (travel)
+	if (*h == NULL && idx == 0)
 	{
-		count++;
-		if (count == idx)
-			break;
-		travel = travel->next;
-	}
-	if (travel == NULL)
+		*h = new_node;
 		return (NULL);
+	}
+	if (idx == 0)
+	{
+		new_node->next = *h;
+		(*h)->prev = new_node;
+		*h = new_node;
+	}
 	else
 	{
-		current = travel->next;
-		travel->next = new_node;
-		current->prev = new_node;
-		new_node->next = current;
-		new_node->prev = travel;
+		travel = *h;
+		while (travel && count < idx - 1)
+		{
+			count++;
+			travel = travel->next;
+		}
+		if (travel == NULL)
+			return (NULL);
+		if (travel->next == NULL)
+		{
+			travel->next = new_node;
+			new_node->prev = travel;
+		}
+		else
+		{
+			new_node->next = travel->next;
+			new_node->prev = travel;
+			travel->next->prev = new_node;
+			travel->next = new_node;
+		}
 	}
 	return (new_node);
 }
