a5d05a2dfe5d967705640f621340b81f97e5b8f8
Wasealex <wasealex27@gmail.com>
2023-12-07 19:38:43 +0300

    fixed betty and *h

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,16 +12,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int count = 0;
 
 	new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL)
+	if (new_node == NULL || *h == NULL)
 		return (NULL);
 	new_node->prev = NULL;
 	new_node->n = n;
 	new_node->next = NULL;
-	if (*h == NULL && idx == 0)
-	{
-		*h = new_node;
-		return (NULL);
-	}
 	if (idx == 0)
 	{
 		new_node->next = *h;
