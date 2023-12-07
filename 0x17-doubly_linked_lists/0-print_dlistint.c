f34ec4642e5a5cfdaee44a87e7e872692a49053a
Wasealex <wasealex27@gmail.com>
2023-12-07 14:31:22 +0300

    betty fix

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,8 +8,9 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	size_t count = 0;
 	const dlistint_t *current = NULL;
+
 	current = h;
-        while (current != NULL)
+	while (current != NULL)
 	{
 		printf("%d\n", current->n);
 		count++;
