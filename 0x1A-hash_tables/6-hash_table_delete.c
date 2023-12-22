177401b9c619b3ff6567f6862e849bf05223f55e
Wasealex <wasealex27@gmail.com>
2023-12-22 11:55:27 +0300

    function that delete the whole table

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+/**
+ *hash_table_delete - delete the table
+ *@ht: the table
+ *Return: void
+ */
+void hash_table_delete(const hash_table_t *ht)
+{
+	hash_node_t *current, *temp;
+	unsigned long int i;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		current = ht->array[i];
+		while (current != NULL)
+		{
+			temp = current;
+			current = current->next;
+			free(temp->key);
+			free(temp->value);
+			free(temp);
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
