ce393f60d0b7d02faa1529c6b55a4eb74d49f8ef
Wasealex <wasealex27@gmail.com>
2023-12-22 12:24:17 +0300

    betty fix

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -17,10 +17,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	key_copy = strdup(key);
 	value_copy = strdup(value);
 	if (value_copy == NULL)
-	{
-		free(value_copy);
 		return (0);
-	}
 	new_pair = malloc(sizeof(hash_node_t));
 	if (new_pair == NULL)
 		return (0);
